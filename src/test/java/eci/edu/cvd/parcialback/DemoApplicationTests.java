714c1c03547d813d6fd07afd28220cc0ecb2616c
Krayangel <david.villadiego-m@mail.escuelaing.edu.co>
2025-03-27 12:56:02 -0500

    springintlzr

---
+++ src/test/java/eci/edu/cvd/parcialback/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.cvd.parcialback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
